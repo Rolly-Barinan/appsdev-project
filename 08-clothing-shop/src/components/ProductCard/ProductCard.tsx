8fcbece0413a36e09c25b349a01afdafd202badb
rolly barinan <rolly.barinan.21@usjr.edu.ph>
2022-12-08 18:54:49 +0800

    updated productCard

--- 08-clothing-shop/src/components/ProductCard/ProductCard.tsx
+++ 08-clothing-shop/src/components/ProductCard/ProductCard.tsx
@@ -1,23 +1,42 @@
-import {
-    AddButton,
-    SubTitle,
-    TextContainer,
-    Title,
-    Wrapper,
-  } from './ProductCard.styled';
+import { AddButton, SubTitle,TextContainer,Title,Wrapper,} from './ProductCard.styled';
+
+import { useState, useEffect, useContext } from 'react';
+import { Product } from '../../models';
+import { ClothingShopContext} from '../../useContext';
+
+export const ProductCard = ({ name, imageUrl, price }: Product) => {
+  const {products, addToCart, removeItem} = useContext(ClothingShopContext);
+  const [isInCart, setIsInCart] = useState(false);
   
-  import { Product } from '../../models';
+  useEffect(() => {
+    const itemInCart = products.find((product: { name: string; }) => product.name === name);
+
+    if (itemInCart) {
+      setIsInCart(true);
+    } else {
+      setIsInCart(false);
+    }
+  }, [products, name]);
   
-  export const ProductCard = ({ name, imageUrl, price }: Product) => {
-    return (
-      <Wrapper background={imageUrl}>
-        <AddButton isInCart={false} onClick={() => console.log('Implement Me')}>
-          <p>+</p>
-        </AddButton>
-        <TextContainer>
-          <Title>{name}</Title>
-          <SubTitle>{price}.00$</SubTitle>
-        </TextContainer>
-      </Wrapper>
-    );
-  };
+  const handleClick = () => {
+    const product = {name, imageUrl, price};
+    if(isInCart){
+      removeItem(product);
+      setIsInCart(false);
+    } else{
+      addToCart(product);
+      setIsInCart(true);
+    }
+  }
+  return (
+    <Wrapper background={imageUrl}>
+      <AddButton isInCart={isInCart} onClick={handleClick}>
+        <p>{isInCart? "-" : "+"}</p>
+      </AddButton>
+      <TextContainer>
+        <Title>{name}</Title>
+        <SubTitle>{price}.00$</SubTitle>
+      </TextContainer>
+    </Wrapper>
+  );
+};
